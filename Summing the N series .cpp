c972aa55bc0fb236df3a711a1af1e652e57b6c33
Abdulrahman Elbanna <109825826+Abdulrahman-PROG@users.noreply.github.com>
2024-04-13 16:05:08 +0200

    Update Summing the N series .cpp

--- Summing the N series .cpp
+++ Summing the N series .cpp
@@ -1,19 +1,19 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
+#define e "\n"
+#define ll long long
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
 
 int main() {
-    unsigned long long int t, n, res;
+    FAST;
+    unsigned ll int t, n, res;
     cin >> t;
     while(t--){
         cin >> n;
         n %= 1000000007;
         res = n*n % 1000000007;
-        cout << res << endl;
+        cout << res << e;
     }
     return 0;
 }
