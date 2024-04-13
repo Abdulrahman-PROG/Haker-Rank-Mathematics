756149b7e6aeb45787ccc4b1a13adffc2c38e00e
Abdulrahman Elbanna <109825826+Abdulrahman-PROG@users.noreply.github.com>
2024-04-13 15:50:16 +0200

    Update Even Odd Query.cpp

--- Even Odd Query.cpp
+++ Even Odd Query.cpp
@@ -1,10 +1,13 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
+#define e "\n"
+#define ll long long
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 int A[100001];
 
 int main()
 {
+    FAST;
     int n;
     cin >> n;
     for (int i = 1; i <= n; i++)
@@ -16,9 +19,9 @@ int main()
         int x, y;
         cin >> x >> y;
         if ((x + 1 <= n) && (A[x + 1] == 0) && (x != y))
-            cout << "Odd" << endl;
+            cout << "Odd" << e;
         else
-            cout << (A[x] % 2 == 0 ? "Even" : "Odd") << endl;
+            cout << (A[x] % 2 == 0 ? "Even" : "Odd") << e;
     }
     return 0;
 }
