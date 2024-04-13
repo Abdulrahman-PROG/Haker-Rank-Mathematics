855ce4362df435731b65eb7caf41e47f51e4b1f4
Abdulrahman Elbanna <109825826+Abdulrahman-PROG@users.noreply.github.com>
2024-04-13 15:53:20 +0200

    Update K Candy Store.cpp

--- K Candy Store.cpp
+++ K Candy Store.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
+#define e "\n"
+#define ll long long
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
 const int MOD = 1000000000;
 vector<vector<int>> binomialCoefficients(2001, vector<int>(2001, 0));
@@ -14,13 +16,14 @@ void calculateBinomialCoefficients() {
 }
 
 int main() {
+    FAST;
     calculateBinomialCoefficients();
     int t;
     cin >> t;
     while (t--) {
         int n, k;
         cin >> n >> k;
-        cout << binomialCoefficients[n + k - 1][n - 1] << endl;
+        cout << binomialCoefficients[n + k - 1][n - 1] << e;
     }
     return 0;
 }
